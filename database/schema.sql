20a4996e61b06c8726c2083b5b35acec79717afa
jekku <jekripreclaroorlina@gmail.com>
2016-09-11 12:22:34 +0800

    Updates - Follow specs for fix and solve issue #3

---
+++ database/schema.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS user_app;
+CREATE DATABASE user_app;
+
+USE user_app;
+
+/*
+    RULES:
+
+    For any table, it must have:
+        - date_created
+        - date_updated
+    For tracking reasons
+
+    Use InnoDB to avoid full table level locks
+*/
+
+/*
+    Use UUID for ID to guarantee uniqueness.
+    username arbitrary max length set to 36.
+    sha256 password is always 64 in length.
+    Max email address length is 254.
+ */
+
+CREATE TABLE users (
+    id varchar(36) NOT NULL PRIMARY KEY,
+    username varchar(36) NOT NULL UNIQUE,
+    password varchar(64) NOT NULL,
+    salt varchar(64) NOT NULL,
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    email_address varchar(254) NOT NULL,
+    date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    date_updated DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
