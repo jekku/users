a922b8b1639c40c35f64e338aa3176a658c3661e
Jekri Preclaro Orlina <jekku@users.noreply.github.com>
2016-09-11 12:23:39 +0800

    (Issue #3) Create a schema for the users table
    - Follow specs for fix and solve issue #3

---
+++ database/seed.sql
@@ -0,0 +1,20 @@
+use user_app;
+
+INSERT INTO users (
+    id,
+    username,
+    password,
+    salt,
+    first_name,
+    last_name,
+    email_address
+) VALUES (
+    'c8efb04f-ca4b-4896-9a02-71b766b7e3c0',
+    'user',
+    '5ad713c4ae3546948fdabf33d4e99ae58fa639280dc9c5e3028d6ed99a08e8d5',
+    '8eWidLg6KWgnNachMtbg9vuetQHiZvR0Y6bVibZ4QvFIpOWo7Wm6fjz69P90xM7t',
+    'Javier',
+    'Alcázar',
+    'javier@isr.co.jp'
+);
+
