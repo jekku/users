2cfad09d6984694644e2b3b1c0856a4f20f4f18f
jekku <jekripreclaroorlina@gmail.com>
2016-09-14 11:17:47 +0800

    Updates - Update test cases

--- test/unit/helpers/PasswordHelper.js
+++ test/unit/helpers/PasswordHelper.js
@@ -4,36 +4,38 @@ import * as PasswordHelper from '../../../helpers/PasswordHelper';
 
 export default function () {
     describe('PasswordHelper', () => {
-        it(`Random string generator should return a string of length 64 by default`,
-            (done) => {
-                const randomString = PasswordHelper.getRandomString();
-                randomString.length.should.equal(64);
-                done();
-            }
-        );
+        describe('Random String Generator', () => {
+            it(`Random string generator should return a string of length 64 by default`,
+                (done) => {
+                    const randomString = PasswordHelper.getRandomString();
+                    randomString.length.should.equal(64);
+                    done();
+                }
+            );
 
-        it(`Random string generator should return a string of length N with a parameter`,
-            (done) => {
-                const randomLength = Math.floor(Math.random() * 100);
-                const randomString = PasswordHelper.getRandomString(randomLength);
-                randomString.length.should.equal(randomLength);
-                done();
-            }
-        );
-    });
+            it(`Random string generator should return a string of length N with a parameter`,
+                (done) => {
+                    const randomLength = Math.floor(Math.random() * 100);
+                    const randomString = PasswordHelper.getRandomString(randomLength);
+                    randomString.length.should.equal(randomLength);
+                    done();
+                }
+            );
+        });
 
-    describe('Encryptor', () => {
-        it(`Encryptor should make a reliable salted  hash for the string in sha256`,
-            (done) => {
-                const presetHashed  = '5ad713c4ae3546948fdabf33d4e99ae58fa639280dc9c5e3028d6ed99a08e8d5';
-                const arbitrarySalt = '8eWidLg6KWgnNachMtbg9vuetQHiZvR0Y6bVibZ4QvFIpOWo7Wm6fjz69P90xM7t'
-                const encryptedString = PasswordHelper.encrypt('user', arbitrarySalt);
+        describe('Encryptor', () => {
+            it(`Encryptor should make a reliable salted  hash for the string in sha256`,
+                (done) => {
+                    const presetHashed  = '5ad713c4ae3546948fdabf33d4e99ae58fa639280dc9c5e3028d6ed99a08e8d5';
+                    const arbitrarySalt = '8eWidLg6KWgnNachMtbg9vuetQHiZvR0Y6bVibZ4QvFIpOWo7Wm6fjz69P90xM7t'
+                    const encryptedString = PasswordHelper.encrypt('user', arbitrarySalt);
 
-                encryptedString.should.equal(presetHashed);
+                    encryptedString.should.equal(presetHashed);
 
-                done();
-            }
-        );
+                    done();
+                }
+            );
+        });
     });
 };
 
