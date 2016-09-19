69c1cf928dba573f1db0c62560557b0a1aa09fb9
Jekri Preclaro Orlina <jekku@users.noreply.github.com>
2016-09-19 14:45:18 +0800

    Launch Integration Fixes
    - fix launch integrations and bad config files

--- frontend/env/prod_env.js
+++ frontend/env/prod_env.js
@@ -1,6 +1,6 @@
 (function (window) {
     window.__env = window.__env || {};
-    window.__env.apiUrl = 'http://philands.cloudap.net:3000';
+    window.__env.apiUrl = 'http://philands.cloudapp.net:3000';
     window.__env.baseUrl = '/api';
 }(this));
 
